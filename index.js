39da8fadce6dfd4a34a4066c817931c6900ec3aa
Benjamin Heintz <heintz.benjamin@gmail.com>
2018-11-07 22:02:06 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var swaggerUi = require('swagger-ui-express');
+var parseSchemaToSwagger = require('./schema-to-swagger').parseSchemaToSwagger;
+var request = require('request-promise');
+var parseBaseSwaggerSpec = require('./parse-swagger-base.json');
+
+/**
+ * constructor
+ * @returns app: express middleware
+ */
+function ParseSwagger(options) {
+    this.config = options;
+
+    var app = express();
+
+    var swagOpts = { swaggerUrl: this.config.host + '/api-docs' };
+    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(null, swagOpts));
+    app.use('/api-docs', this.renderSwaggerSpec.bind(this));
+
+    return app;
+};
+
+/**
+ * Get parse compatible api swagger.json base
+ */
+ParseSwagger.prototype.renderSwaggerSpec = function (req, res) {
+
+    var options = {
+        url: this.config.host + this.config.apiRoot + '/schemas',
+        method: 'GET',
+        json: true,
+        headers: { "X-Parse-Application-Id": this.config.appId, "X-Parse-Master-Key": this.config.masterKey }
+    };
+
+    request(options).then((data) => {
+        var swagger = parseSchemaToSwagger(parseBaseSwaggerSpec, data.results);
+        res.json(swagger);
+    }).catch((error) => {
+        res.send('Request failed with response code ' + error.status);
+    });
+}
+
+module.exports = ParseSwagger;
---
+++ readme.md
@@ -0,0 +1,38 @@
+parse-server-swagger
+====================
+
+Autogenerate parse server API swagger.json and swagger UI from Parse Server schemas
+
+Installation
+------------
+
+        npm i parse-server-swagger -S
+
+How to use ?
+------------
+
+``` js
+
+var ParseSwagger = require('./parse-server-swagger');
+
+// Serve swagger API documentation
+if (CONFIG.swagger) {
+    var parseSwagger = new ParseSwagger({
+        host: 'http:localhost:1337',
+        parsePath: '/parse'
+        appId: '<appId>',
+        masterKey: '<masterKey>',
+    });
+
+    app.use(parseSwagger);
+}
+
+```
+
+Expose swagger UI here: 
+    
+    '/swagger'
+
+Expose swagger.json here:
+    
+    '/api-docs'
